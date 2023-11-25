37bd1383225691f89e0c6897dde1bf640782ad46
Guna-1527 <126180469+Guna-1527@users.noreply.github.com>
2023-11-25 14:43:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,13 @@ const Task = require("./models/Task");
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["todo-cli-gunas-projects-65696d9d.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }
+));
 
 mongoose
     .connect(
